cc69fbf51c44b5b179bc0770e605288753842caf
likesh k <110963658+DIET-co@users.noreply.github.com>
2024-03-04 12:39:56 +0530

    dele

--- C-Programing/codesnippet/month.c
+++
@@ -1,2 +0,0 @@
-//month name
-char *m[]={"January","February","March","April","May","June","July","August","September","October","November","December"};
--- C-Programing/codesnippet/swap_value_XOR.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-inline void swap(a,b){a^=b;b^=a;a^=b;}
-
-int main(){
-	int x=10,y=20;
-	swap(x,y);
-	printf("%d %d\n",x,y);
-	return 0;
-}
